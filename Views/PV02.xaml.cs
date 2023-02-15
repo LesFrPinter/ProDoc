b6f849f0eb3aa5e349a751ef214ad8b8887085c5
Les Pinter <Les@LPinter.com>
2023-02-15 10:13:38 -0800

    Finished Cost Summary TabPage

--- Views/PV02.xaml.cs
+++ Views/PV02.xaml.cs
@@ -12,6 +12,8 @@ namespace ProDocEstimate.Views
         {
             InitializeComponent();
 						LoadData();
+						LoadSummaryData();
+						LoadOtherData();
 				}
 
 		public event PropertyChangedEventHandler? PropertyChanged;
@@ -21,6 +23,20 @@ namespace ProDocEstimate.Views
 		{
 			get { return est; }
 			set { est = value; /*OnPropertyChanged(new DependencyPropertyChangedEventArgs());*/ }
+		}
+
+		private DataTable summary;
+		public DataTable Summary
+		{
+			get { return summary; }
+			set { summary = value; }
+		}
+
+		private DataTable otherData;
+		public DataTable OtherData
+		{
+			get { return otherData; }
+			set { otherData = value; }
 		}
 
 		private void LoadData()
@@ -38,8 +54,43 @@ namespace ProDocEstimate.Views
 			this.Est.Rows.Add("14", "PACKAGING",          "41.75", " 1.11");
 			this.Est.Rows.Add("15", "11-1-1 STD COLOR",   "41.75", " 0.60");
 
-			this.MyGrid.DataContext = this.Est;
+			//this.MyGrid.DataContext = this.Est;
 			this.MyGrid.ItemsSource = Est.DefaultView;
+		}
+
+		private void LoadSummaryData()
+		{
+			this.Summary = new DataTable("Summary");
+			this.Summary.Columns.Add("Paper");
+			this.Summary.Columns.Add("LbsReq");
+			this.Summary.Columns.Add("WastePct");
+			this.Summary.Columns.Add("CostPerLb");
+			this.Summary.Columns.Add("ExtCost");
+
+			this.Summary.Rows.Add("   9 1/2  26.0# WHITE   PV - CB COATED BACK NCR", "159", "9.00", "1.505", "238.36");
+			this.Summary.Rows.Add("   9 1/2  14.0# BLUE    PV - CFB COATED FRT/BACK", "83", "8.00", "1.673", "138.68");
+			this.Summary.Rows.Add("   9 1/2  15.0# PINK    PV - CF COATED FRONT NCR", "92", "9.00", "0.9455", "86.36");
+
+			//this.dgSummary.DataContext = this.Summary;
+			this.dgSummary.ItemsSource = Summary.DefaultView;
+		}
+
+		private void LoadOtherData()
+		{
+			this.OtherData = new DataTable("OtherData");
+			this.OtherData.Columns.Add("Department");
+			this.OtherData.Columns.Add("SetupTime");
+			this.OtherData.Columns.Add("SetupCost");
+			this.OtherData.Columns.Add("RunTime");
+			this.OtherData.Columns.Add("RunCost");
+
+			this.OtherData.Rows.Add("PRESS",    "0.60", "39.00", "1.410", "91.85");
+			this.OtherData.Rows.Add("COLLATOR", "0.35", "14.00", "0.863", "34.52");
+			this.OtherData.Rows.Add("BINDERY",  "0.00", "0.00",  "1.410", "0.00");
+			this.OtherData.Rows.Add("PREP",     "0.00", "0.00",  "", "IMPR: 15060    ");
+
+			//this.dgDetails.DataContext = this.OtherData;
+			this.dgDetails.ItemsSource = OtherData.DefaultView;
 		}
 
 	}
