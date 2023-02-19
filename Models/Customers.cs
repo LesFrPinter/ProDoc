2b4c13b4f74e89be378b2666146a889b92ff21fb
Les Pinter <Les@LPinter.com>
2023-02-19 13:33:47 -0800

    CUSTOMERS class corrections

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -5,12 +5,14 @@ namespace ProDocEstimate.ViewModels
 	public class Customers
 	{
 		public Guid ID { get; set; }
-		public string? CustomerName { get; set; }
-		public string? Address { get; set; }
-		public string? City { get; set; }
-		public string? State { get; set; }
+		public string? CUST_NUMB { get; set; }
+		public string? CUST_NAME { get; set; }
+		public string? ADDRESS1 { get; set; }
+		public string? ADDRESS2 { get; set; }
+		public string? CITY { get; set; }
+		public string? STATE { get; set; }
 		public string? ZIP { get; set; }
-		public string? Phone { get; set; }
-		public string? Email { get; set; }
+		public string? ATTENTION { get; set; }
+		public string? PHONE { get; set; }
 	}
 }
