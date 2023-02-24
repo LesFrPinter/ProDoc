baa607327ddad5cff2317ac731a19850e340699b
Les Pinter <Les@LPinter.com>
2023-02-23 17:27:21 -0800

    Finished layout of Collator Standards

--- Views/Standards.xaml.cs
+++ Views/Standards.xaml.cs
@@ -46,31 +46,66 @@ namespace ProDocEstimate.Views
 		private string? ftPerMin7; public string? FtPerMin7 { get { return ftPerMin7; } set { ftPerMin7 = value; OnPropertyChanged(); } }
 		private string? ftPerMin8; public string? FtPerMin8 { get { return ftPerMin8; } set { ftPerMin8 = value; OnPropertyChanged(); } }
 
-		private string? eclSetup;     public string? ECLSetup			{ get { return eclSetup;		 } set { eclSetup			= value; OnPropertyChanged(); } }
+//		private string? eclSetup;     public string? ECLSetup			{ get { return eclSetup;		 } set { eclSetup			= value; OnPropertyChanged(); } }
 		private string? runCost;      public string? RunCost			{ get { return runCost;			 } set { runCost			= value; OnPropertyChanged(); } }
 		private string? materialCost; public string? MaterialCost { get { return materialCost; } set { materialCost = value; OnPropertyChanged(); } }
 
 		// Feature Standards properties
 
-		//	private string? web; public string? Web { get { return web; } set { web = value; OnPropertyChanged(); } }
-		//	private string? web; public string? Web { get { return web; } set { web = value; OnPropertyChanged(); } }
+		//	private string? alert; public string? Alert { get { return alert; } set { alert = value; OnPropertyChanged(); } }
+		//	
 
 		private string? featureStandardNum; public string? FeatureStandardNum { get { return featureStandardNum; } set { featureStandardNum = value; OnPropertyChanged(); } }
-		private string? featureNum; public string? FeatureNum { get { return featureNum; } set { featureNum = value; OnPropertyChanged(); } }
+		private string? featureNum;      public string? FeatureNum { get { return featureNum; } set { featureNum = value; OnPropertyChanged(); } }
 		private string? featureDescription; public string? FeatureDescription { get { return featureDescription; } set { featureDescription = value; OnPropertyChanged(); } }
-		private string? sort; public string? Sort { get { return sort; } set { sort = value; OnPropertyChanged(); } }
-		private string? flatPricing; public string? FlatPricing { get { return flatPricing; } set { flatPricing = value; OnPropertyChanged(); } }
-		private string? runPricing; public string? RunPricing { get { return runPricing; } set { runPricing = value; OnPropertyChanged(); } }
-		private string? priceByInch; public string? PriceByInch { get { return priceByInch; } set { priceByInch = value; } }
-		private string? priceBySqInch; public string? PriceBySqInch { get { return priceBySqInch; } set { priceBySqInch = value; } }
-		private string? sellDollars; public string? SellDollars { get { return sellDollars; } set { sellDollars = value; OnPropertyChanged(); } }
-		private string? costDollars; public string? CostDollars { get { return costDollars; } set { costDollars = value; OnPropertyChanged(); } }
-		private string? costMU1; public string? CostMU1 { get { return costMU1; } set { costMU1 = value; OnPropertyChanged(); } }
-		private string? costMU2; public string? CostMU2 { get { return costMU2; } set { costMU2 = value; OnPropertyChanged(); } }
-		private string? pressTime; public string? PressTime { get { return pressTime; } set { pressTime = value; OnPropertyChanged(); } }
-		private string? pressMaterial; public string? PressMaterial { get { return pressMaterial; } set { pressTime = value; OnPropertyChanged(); } }
-		private string? pressSlowdown; public string? PressSlowdown { get { return pressSlowdown; } set { pressSlowdown = value; OnPropertyChanged(); } }
+		private string? sort;            public string? Sort { get { return sort; } set { sort = value; OnPropertyChanged(); } }
+		private string? flatPricing;     public string? FlatPricing { get { return flatPricing; } set { flatPricing = value; OnPropertyChanged(); } }
+		private string? runPricing;      public string? RunPricing { get { return runPricing; } set { runPricing = value; OnPropertyChanged(); } }
+		private string? priceByInch;     public string? PriceByInch { get { return priceByInch; } set { priceByInch = value; } }
+		private string? priceBySqInch;   public string? PriceBySqInch { get { return priceBySqInch; } set { priceBySqInch = value; } }
+		private string? sellDollars;     public string? SellDollars { get { return sellDollars; } set { sellDollars = value; OnPropertyChanged(); } }
+		private string? costDollars;     public string? CostDollars { get { return costDollars; } set { costDollars = value; OnPropertyChanged(); } }
+		private string? costMU1;         public string? CostMU1 { get { return costMU1; } set { costMU1 = value; OnPropertyChanged(); } }
+		private string? costMU2;         public string? CostMU2 { get { return costMU2; } set { costMU2 = value; OnPropertyChanged(); } }
 
+		private string? prepTime; public string? PrepTime { get { return prepTime; } set { prepTime = value; OnPropertyChanged(); } }
+		private string? prepMaterial; public string? PrepMaterial { get { return prepMaterial; } set { prepMaterial = value; OnPropertyChanged(); } }
+		private string? prepSlowdown; public string? PrepSlowdown { get { return prepSlowdown; } set { prepSlowdown = value; OnPropertyChanged(); } }
+
+		private string? pressTime;       public string? PressTime { get { return pressTime; } set { pressTime = value; OnPropertyChanged(); } }
+		private string? pressMaterial;   public string? PressMaterial { get { return pressMaterial; } set { pressMaterial = value; OnPropertyChanged(); } }
+		private string? pressSlowdown;   public string? PressSlowdown { get { return pressSlowdown; } set { pressSlowdown = value; OnPropertyChanged(); } }
+
+		private string? colorTime;       public string? ColorTime { get { return colorTime; } set { colorTime = value; OnPropertyChanged(); } }
+		private string? colorMaterial;   public string? ColorMaterial { get { return colorMaterial; } set { colorMaterial = value; OnPropertyChanged(); } }
+		private string? colorSlowdown;   public string? ColorSlowdown { get { return colorSlowdown; } set { colorSlowdown = value; OnPropertyChanged(); } }
+		private string? bindCost;        public string? BindCost { get { return bindCost; } set { bindCost = value; OnPropertyChanged(); } }
+
+		private string? otherTime;       public string? OtherTime { get { return otherTime; } set { otherTime = value; OnPropertyChanged(); } }
+		private string? otherMaterial;   public string? OtherMaterial { get { return otherMaterial; } set { otherMaterial = value; OnPropertyChanged(); } }
+		private string? oneTimeMaterial; public string? OneTimeMaterial { get { return oneTimeMaterial; } set { oneTimeMaterial = value; OnPropertyChanged(); } }
+		private string? whatsThis;       public string? WhatsThis { get { return whatsThis; } set { whatsThis = value; OnPropertyChanged(); } }
+
+		private string? optNumAround;    public string? OptNumAround { get { return optNumAround; } set { optNumAround = value; OnPropertyChanged(); } }
+		private string? optPart;         public string? OptPart { get { return optPart; } set { optNumAround = value; OnPropertyChanged(); } }
+		private string? optStream;       public string? OptStream { get { return optStream; } set { optStream = value; OnPropertyChanged(); } }
+		private string? askNumFlat;      public string? AskNumFlat { get { return askNumFlat; } set { askNumFlat = value; OnPropertyChanged(); } }
+		private string? askNumFlatDesc;  public string? AskNumFlatDesc { get { return askNumFlatDesc; } set { askNumFlatDesc = value; OnPropertyChanged(); } }
+		private string? askNumRun;       public string? AskNumRun { get { return askNumRun; } set { askNumRun = value; OnPropertyChanged(); } }
+		private string? askNumRunDesc;   public string? AskNumRunDesc { get { return askNumRunDesc; } set { askNumRunDesc = value; OnPropertyChanged(); } }
+		private string? optPercent;      public string? OptPercent { get { return optPercent; } set { optPercent = value; OnPropertyChanged(); } }
+		private string? optType;         public string? OptType { get { return optType; } set { optType = value; OnPropertyChanged(); } }
+		private string? eCLSetup;        public string? ECLSetup { get { return eCLSetup; } set { eCLSetup = value; OnPropertyChanged(); } }
+		private string? eCLRun;          public string? ECLRun { get { return eCLRun; } set { web = value; OnPropertyChanged(); } }
+		private string? eCLMaterial;     public string? ECLMaterial { get { return eCLMaterial; } set { web = value; OnPropertyChanged(); } }
+		private string? sets;            public string? Sets { get { return sets; } set { sets = value; OnPropertyChanged(); } }
+		private bool?   multStreamOK;    public bool?   MultStreamOK { get { return multStreamOK; } set { multStreamOK = value; OnPropertyChanged(); } }
+		private string? wasteSetup;      public string? WasteSetup { get { return wasteSetup; } set { wasteSetup = value; OnPropertyChanged(); } }
+		private string? wasteRunPct;     public string? WasteRunPct { get { return wasteRunPct; } set { wasteRunPct = value; OnPropertyChanged(); } }
+		private string? mRImpressions;   public string? MRImpressions { get { return mRImpressions; } set { mRImpressions = value; OnPropertyChanged(); } }
+		private string? cartonQty;       public string? CartonQty { get { return cartonQty; } set { cartonQty = value; OnPropertyChanged(); } }
+		private string? calcType;        public string? CalcType { get { return calcType; } set { calcType = value; OnPropertyChanged(); } }
+		private string? alert;           public string? Alert { get { return alert; } set { alert = value; OnPropertyChanged(); } }
 
 		public Standards()
       {
@@ -122,7 +157,58 @@ namespace ProDocEstimate.Views
 			RunCost = "1";
 			MaterialCost = "1";
 
-		}
+			FeatureStandardNum = "02";
+			FeatureNum = "03";
+			FeatureDescription = "Description goes here";
+			Sort = "A";
+			FlatPricing = "2";
+			RunPricing = "2";
+			PriceByInch = "2";
+			PriceBySqInch = "2";
+			SellDollars = "2";
+			CostDollars = "2";
+			CostMU1 = "2";
+			CostMU2 = "";
+
+			PrepTime = "2";
+			PrepMaterial = "2";
+			PrepSlowdown = "2";
+
+			PressTime = "2";
+			PressMaterial = "2";
+			PressSlowdown = "2";
+
+			ColorTime = "2";
+			ColorMaterial = "2";
+			ColorSlowdown = "2";
+			BindCost = "2";
+
+			OtherTime = "2";
+			OtherMaterial = "2";
+			OneTimeMaterial = "2";
+			WhatsThis = "2";
+
+			OptNumAround = "2";
+			OptPart = "2";
+			OptStream = "2";
+			AskNumFlat = "2";
+			AskNumFlatDesc = "2";
+			AskNumRun = "2";
+			AskNumRunDesc = "2";
+			OptPercent = "2";
+			OptType = "2";
+			ECLSetup = "2";
+			ECLRun = "2";
+			ECLMaterial = "2";
+			Sets = "2";
+			MultStreamOK = true;
+			WasteSetup = "22";
+			WasteRunPct = "2";
+			MRImpressions = "2";
+			CartonQty = "2";
+			CalcType = "2";
+			Alert = "Where does this come from?";
+	}
 
 		public event PropertyChangedEventHandler? PropertyChanged;
 		protected void OnPropertyChanged([CallerMemberName] string? name = null)
